368d3410614ded8f6b1e47293789a460f8ea3cf0
ilya smagin <ilya-z1999@ya.ru>
2023-07-09 23:55:48 +0300

    Select all posts link fix

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -18,7 +18,7 @@ export async function action({ request }) {
 	const formData = await request.formData();
 	const filterId = formData.get("userId");
 	if (filterId === "0") {
-		return redirect("/posts");
+		return redirect("./posts");
 	} else {
 		return redirect(`posts/?userId=${filterId}`);
 	}
@@ -31,41 +31,41 @@ export default function Root() {
 
 	return (
 		<>
-				<header className="flex flex-row justify-between p-4">
-					<Link
-						to="./posts"
-						className="text-fuchsia-700 text-3xl font-bold"
+			<header className="flex flex-row justify-between p-4">
+				<Link
+					to="./posts"
+					className="text-fuchsia-700 text-3xl font-bold"
+				>
+					Picasso blog
+				</Link>
+				<Form
+					method="post"
+					name="user"
+					className="w-fit flex flex-row items-baseline mr-8"
+				>
+					<label
+						htmlFor="userSelect"
+						className="min-w-fit mb-2 mr-1 text-sm"
 					>
-						Picasso blog
-					</Link>
-					<Form
-						method="post"
-						name="user"
-						className="w-fit flex flex-row items-baseline mr-8"
+						Posts by
+					</label>
+					<select
+						onChange={(e) => submit(e.currentTarget.form)}
+						id="userSelect"
+						name="userId"
+						value={userId}
+						className="block pt-2.5 w-full min-w-fit text-sm bg-transparent border-0 border-b-[1px] border-fuchsia-800 appearance-none focus:outline-none focus:ring-0 focus:border-fuchsia-600 peer"
 					>
-						<label
-							htmlFor="userSelect"
-							className="min-w-fit mb-2 mr-1 text-sm"
-						>
-							Posts by
-						</label>
-						<select
-							onChange={(e) => submit(e.currentTarget.form)}
-							id="userSelect"
-							name="userId"
-							value={userId}
-							className="block pt-2.5 w-full min-w-fit text-sm bg-transparent border-0 border-b-[1px] border-fuchsia-800 appearance-none focus:outline-none focus:ring-0 focus:border-fuchsia-600 peer"
-						>
-							<option value="0">all users</option>
-							{users.map((user) => (
-								<option key={user.id} value={user.id}>
-									{user.name}
-								</option>
-							))}
-						</select>
-					</Form>
-				</header>
-				<Outlet />
+						<option value="0">all users</option>
+						{users.map((user) => (
+							<option key={user.id} value={user.id}>
+								{user.name}
+							</option>
+						))}
+					</select>
+				</Form>
+			</header>
+			<Outlet />
 		</>
 	);
 }
